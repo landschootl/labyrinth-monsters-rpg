579e770412ae503f1e6e117d1204882aa9ae6e7c
Ludovic <ludovic.isn@gmail.com>
2015-09-19 16:42:35 +0200

    Avancement sur la console.

--- src/console/Console.java
+++ src/console/Console.java
@@ -1,12 +1,25 @@
 package console;
 
+import org.jsfml.graphics.Color;
+import org.jsfml.graphics.RectangleShape;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.system.Vector2f;
 import org.jsfml.window.event.Event;
 
 public class Console {
+	private RectangleShape background;
 	
 	public Console(){
-		
+		initBackground();
+	}
+	
+	// Fonction qui initialise le background de la console.
+	private void initBackground() {
+		background = new RectangleShape(new Vector2f(272,120));
+		background.setFillColor(new Color(Color.BLACK, 0));
+		background.setOutlineThickness(8);
+		background.setOutlineColor(new Color(Color.WHITE, 0));
+		background.setPosition(new Vector2f(0,680));
 	}
 	
 	// Fonction qui permet de gérer les événements.
@@ -21,6 +34,6 @@ public class Console {
 
 	// Fonction qui permet d'afficher le rendu graphique dans la fenetre.
 	public void render(RenderWindow window) {
-		
+		window.draw(background);
 	}
 }
--- src/resource/Text.java
+++ src/resource/Text.java
@@ -1,5 +1,5 @@
 package resource;
 
 public class Text {
-
+	
 }
